055e219ea1b949e0d438ad6d0ef0ca389beebefa
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-03-13 13:48:05 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,17 @@ import {ModuleDefinition} from 'forms42core';
 
 class Main
 {
+    public show() : void
+    {
+        let root:Element = document.body.querySelector('forms');
+        let template:HTMLTemplateElement = document.createElement('template');
 
+        let countries:Countries = new Countries();
+        template.innerHTML = countries.page();
+
+        root.replaceWith(template.content);
+    }
 }
+
+let main:Main = new Main();
+main.show();
