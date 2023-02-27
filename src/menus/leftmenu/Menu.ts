5e8b03b9e714d11e0a0b8ace657fae838d2cb316
ahoeffner <alex@hoeffner.net>
2023-02-27 16:27:46 +0100

    Update Menu.ts

--- src/menus/leftmenu/Menu.ts
+++ src/menus/leftmenu/Menu.ts
@@ -45,6 +45,7 @@ export class Menu extends MenuComponent
 
 	public async hide() : Promise<void>
 	{
+		console.log("hide doesn't work")
 		this.displayed = false;
 		this.container.style.minWidth = "0px";
 		this.container.classList.remove("menu-left-open");
