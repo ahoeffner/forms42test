76c43daedc5d96f563089ec1eb7a2d1ba6f2d45d
ahoeffner <alex@hoeffner.net>
2023-09-25 13:02:23 +0200

    .

--- src/forms/masterdetail/MasterDetail.ts
+++ src/forms/masterdetail/MasterDetail.ts
@@ -45,7 +45,6 @@ export class MasterDetail extends BaseForm
 		this.dept.setLocationLov("location");
 
 		this.emp.setJobLov(["job_id","job_title"]);
-		this.emp.setDepartmentLov(["department_id","department_name"]);
 
 		let empkey:Key = new Key("employees","department_id");
 		let deptkey:Key = new Key("departments","department_id");
---
+++ src/lists/DepartmentsList.ts
@@ -0,0 +1,44 @@
+import { FormsModule } from "../FormsModule";
+import { BindValue, ListOfValues, QueryTable } from "forms42core";
+
+export class DepartmentsList implements ListOfValues
+{
+	public filterPrefix = "%";
+	public filterPostfix = "%";
+
+	public bindvalue: BindValue;
+	public datasource: Departments;
+
+	public displayfields: string;
+	public sourcefields: string[];
+	public targetfields: string[];
+
+	constructor()
+	{
+		this.datasource = new Departments();
+		this.bindvalue = this.datasource.department;
+
+		this.displayfields = "department_name";
+		this.sourcefields = ["department_id","department_name"];
+		this.targetfields = ["department_id","department_name"];
+	}
+}
+
+class Departments extends QueryTable
+{
+	public department:BindValue = new BindValue("department","");
+
+	constructor()
+	{
+		super(FormsModule.DATABASE);
+
+		this.sql =
+		`
+			select * from departments
+			where department_name ilike :department
+		`;
+
+		this.sorting = "department_id";
+		this.addBindValue(this.department);
+	}
+}
